fd7a79087ac25ffd41cc2358ea7925b24060d239
Benjamin Walkenhorst <krylon@posteo.de>
2025-10-07 17:21:02 +0200

    Add boilerplate files

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-10-07 17:22:33 krylon>
+#
+# /data/code/python/pharmacy/__init__.py
+# created on 07. 10. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+pharmacy.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
